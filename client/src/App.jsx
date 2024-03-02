02fdaf7fb0f58b5619533be78ff473954db11573
rafiiwicaksono <rafiihsan5@gmail.com>
2024-03-02 19:33:07 +0700

    client home

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home";
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
